b9bb9e287c8741c21d29bdaa87b28def843918f2
divildomi <domi.adamczewska@gmail.com>
2022-04-26 20:39:51 +0100

    commit

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,78 +1,67 @@
 package ie.tudublin;
 
-
-
-public class Main
-{
-    public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
+public class Main {
+    public static void helloProcessing() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new HelloProcessing());
     }
 
-    public static void starMap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new StarMap());
+    public static void smap() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new smap());
     }
 
-    public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
+    public static void starMap() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new StarMap());
     }
 
-    public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
+    public static void bugZap() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new BugZap());
     }
 
-    public static void arrays()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Arrays());
+    public static void loops() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new Loops());
     }
 
-    public static void life()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Life());
+    public static void arrays() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new Arrays());
     }
 
-    public static void timing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new TimingExample());
+    public static void life() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new Life());
     }
 
-
-    public static void audio1()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Audio1());
+    public static void timing() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new TimingExample());
     }
 
-    public static void audio2()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Audio2());
+    public static void audio1() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new Audio1());
     }
 
-    public static void colorfulLife()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new ColorfulLife());
+    public static void audio2() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new Audio2());
     }
 
-    public static void YASC()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new YASC());
+    public static void colorfulLife() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new ColorfulLife());
     }
 
-    public void cat()
-    {
+    public static void YASC() {
+        String[] a = { "MAIN" };
+        processing.core.PApplet.runSketch(a, new YASC());
+    }
+
+    public void cat() {
         System.out.println("Hello world");
 
         Animal misty = new Animal("Misty");
@@ -84,22 +73,20 @@ public class Main
         System.out.println(misty);
         System.out.println(lucy);
 
-        
         Cat cat = new Cat("Ginger");
 
-        while(cat.getNumLives() > 0)
-        {
+        while (cat.getNumLives() > 0) {
             cat.kill();
         }
         cat.kill();
 
     }
-    public static void main(String[] args)
-    {
-        timing();
+
+    public static void main(String[] args) {
+        smap();
 
         // Tara Misty
         // Tara Tara
-        
+
     }
 }
